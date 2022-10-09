9501ee4e801e50739ebd6c4db23693296555fb68
qwerjava <115363808+qwerjava@users.noreply.github.com>
2022-10-10 01:04:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-DE26OpGd5G8wYbYJDWrHLJN6RQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'rh32DKsph6jOkB6NiuvzeKO6XDU4IAoDZqf6co-sHgU',
+      useTemplateId: 'Ig5SsNwC5cTpcvNq7h39md5YO_E79aK6n7jdXemJcAc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-01',
       festivals: [
