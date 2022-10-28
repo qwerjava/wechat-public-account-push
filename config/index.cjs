2f0c516b0430a8afb2787704cea112bbebe27a90
qwerjava <115363808+qwerjava@users.noreply.github.com>
2022-10-29 03:41:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-DE26Oha1ok1SnIK7syqCel3ln8',
+      id: 'o-DE26OpGd5G8wYbYJDWrHLJN6RQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '1H-oDzHTh_N5BEOTJjpMOKRBc506tJeNabcu12yibzM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
